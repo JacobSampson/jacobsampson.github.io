5d6e34f5b8cd229d42cf4d6b90e6af18c58e70df
Jacob Sampson <jsampson@extroncompany.com>
2019-08-02 08:54:48 -0500

    Added GitHub call support

--- js/main.js
+++ js/main.js
@@ -1,34 +1,87 @@
+class ghRepo {
+    _name;
+    _url;
+    _description;
+    _language;
+    _createdDate;
+
+    constructor(name, url, description, language, createdDate) {
+        this._name = name;
+        this._url = url;
+        this._description = description || null;
+        this._language = language;
+        this._createdDate = createdDate;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get url() {
+        return this._url;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    get language() {
+        return this._language;
+    }
+
+    get createdDate() {
+        return this._createdDate;
+    }
+
+    toString() {
+        let repoString = this._name;
+
+        repoString += '<br>url: ' + this._url;
+        repoString += this._description ? '<br>description: ' + this._description : '';
+        repoString += '<br>language: ' + this._language;
+        repoString += '<br>creation date: ' + this._createdDate;
+
+        return repoString;
+    }
+}
+
 (async () => {
-    let ghContent = document.getElementById("gh-content");
-    
-    let result = await fetch("https://api.github.com/users/JacobSampson/repos");
+    let result = await fetch('https://api.github.com/users/JacobSampson/repos', {
+        headers: {
+            'Authorization': 'token ' + config.AUTH_KEY
+        }
+    });
     let ghRepoInfo = await result.json();
 
     let ghRepos = [];
 
-    ghRepoInfo.forEach(async ghRepo => {
-        let numCommits = await getNumCommits(ghRepo.commits_url);
+    for (repo of ghRepoInfo) {
+        let newRepo = new ghRepo(
+            repo.name,
+            repo.url,
+            repo.description,
+            repo.language,
+            repo.created_at
+        );
 
-        ghRepos.push({
-            name: ghRepo.name,
-            url: ghRepo.url,
-            description: ghRepo.description || "",
-            language: ghRepo.language || "",
-            createdDate: ghRepo.created_at,
-            numCommits: numCommits
-        });
+        ghRepos.push(await newRepo);
+    }
 
-        console.log(ghRepos[ghRepos.length - 1]);
+    return ghRepos;
+})().then((ghRepos) => {
+    let ghContent = document.querySelector('.main-content');
+    ghRepos.forEach(ghRepo => {
+        let newElement = document.createElement('div');
+        newElement.innerHTML = ghRepo.toString();
+        newElement.classList.add('gh-repo');
+        newElement.classList.add('card');
+        
+        console.log(ghRepo);
+
+        ghContent.appendChild(newElement);
     });
-})().catch(() => {
-    console.log("Failure");
+}).catch(() => {
+    console.log('Failure');
 });
 
-async function getNumCommits(url) {
-    let commitInfo = await fetch(url.slice(0, -6));
-    let commits = await commitInfo.json();
-
-    return await commits.length;
-}
-
-// font-family: "Source Sans Pro", Helvetica, sans-serif;
+// font-family: 'Source Sans Pro', Helvetica, sans-serif;
